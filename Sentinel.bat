15f7d469b4d4e6a88bace55c1fb4f5c8e99ccf4c
CortezMaxiI <cortezmaximilianoagustin@gmail.com>
2026-02-02 03:59:45 -0300

    Delete Sentinel.bat

--- Sentinel.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-TITLE SENTINEL-IIOT BOOTLOADER
-:: Ejecuta el script de PowerShell saltándose la política de ejecución local
-powershell -ExecutionPolicy Bypass -File "%~dp0Sentinel-Launcher.ps1"
-pause
